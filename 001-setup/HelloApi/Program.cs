586b7462e6dbe57f2a50c31ba9a2b1cae1902f69
David Liu <david.s.liu@gmail.com>
2023-01-30 13:58:46 -0600

    add HelloApi setup

---
+++ 001-setup/HelloApi/Program.cs
@@ -0,0 +1,66 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo {
+        Version = "v1",
+            Title = "Hello API",
+            Description = "An ASP.NET Core Minimal API",
+            TermsOfService = new Uri("https://example.com/terms"),
+            Contact = new OpenApiContact {
+                Name = "David Liu",
+                    Url = new Uri("https://github.com/dliulabs/")
+            },
+            License = new OpenApiLicense {
+                Name = "MIT License",
+                    Url = new Uri("https://opensource.org/licenses/MIT")
+            }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new [] {
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching"
+};
+
+app.MapGet("/weatherforecast", () => {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast(
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
